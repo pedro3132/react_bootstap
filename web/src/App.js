2c9cacfca15676fc5dc8fb4219d30a01fbb7eb7c
pedro3132 <pedropinheiro.2012@hotmail.com>
2020-01-28 12:43:31 +0000

    Commit

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import menu from './componentes/menu/menu';
+
+
+function App() {
+  return (
+    <>
+    <menu/>
+    </>
+  );
+}
+
+export default App;
