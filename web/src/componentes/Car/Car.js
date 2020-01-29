0a326afd25af0e64d39dc738b842e1b21df25146
pedro3132 <pedropinheiro.2012@hotmail.com>
2020-01-29 14:21:10 +0000

    site

--- web/src/componentes/Car/Car.js
+++ web/src/componentes/Car/Car.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
 import amor from '../imagens/amor.jpg'
 import v from '../Video/v.mp4'
-
+import './car.css'
 
   export default function Car() {
   return (
@@ -17,20 +17,23 @@ import v from '../Video/v.mp4'
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Te amo </h2>
+      <p><strong>E sempre te amarei.</strong></p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <video autoPlay
+    <video controls autoPlay
       className="d-block w-100"
       src={v}
       alt="Third slide"
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h2>Poema</h2>
+      <p>Duvida da luz dos astros,
+De que o sol tenha calor,
+Duvida até da verdade,
+Mas confia em meu amor.</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
--- web/src/componentes/form/form.js
+++ web/src/componentes/form/form.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form } from 'react-bootstrap';
+import {  Button } from 'react-bootstrap';
 
   export default function formu() {
   return (
 <>
-<Form>
-  {['checkbox', 'radio'].map(type => (
-    <div key={`inline-${type}`} className="mb-3">
-      <Form.Check inline label="Sim" type={type} id={`inline-${type}-1`} />
-      <Form.Check inline label="Não" type={type} id={`inline-${type}-2`} />
-      <Form.Check
-        inline
-        disabled
-        label="Numca te amei"
-        type={type}
-        id={`inline-${type}-3`}
-      />
-    </div>
-  ))}
-</Form>
+  <Button href="https://www.youtube.com/watch?v=UybGw37WbuY">Sim</Button>
+  <Button href="https://youtu.be/ofQpO30OBCY?t=31">Não</Button>
+
 </>
   );
 }
